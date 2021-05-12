73f3ff328f5fdb82aa962c33154f4a63e97389cf
rhday <54642787+rhday@users.noreply.github.com>
2021-05-12 16:20:26 +0100

    add styling to logo and build Post.js component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>And so it begins...</h1>
+    <div className="app">
+    <div className="app__header">
+      <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+    </div>
+    <h1>And so it begins...</h1>
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
