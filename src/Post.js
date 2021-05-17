9bc782481c44296096d9e1bd1191a517ce4955c2
rhday <54642787+rhday@users.noreply.github.com>
2021-05-17 19:37:24 +0100

    add CSS styling

--- src/Post.js
+++ src/Post.js
@@ -5,11 +5,14 @@ import Avatar from "@material-ui/core/Avatar";
 function Post() {
     return (
         <div className="post">
-            <Avatar className="post__avatar" alt="Ruari" src="/static/images/avatar/1.jpg" />
-            <h3>Username</h3>
+            <div className="post__header">
+                <Avatar className="post__avatar" alt="Ruari" src="/static/images/avatar/1.jpg" />
+                <h3>Username</h3>
+            </div>
+            
             {/* Header --> avatar + username */}
 
-            <img className="post__image" src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" />
+            <img className="post__image" src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" />
             {/* Image */}
 
             <h4 className="post__text"><strong>rhday</strong> Building instaclone</h4>
