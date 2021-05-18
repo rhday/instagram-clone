9113da789cec70a9bdb096217868ab4df54d217c
rhday <54642787+rhday@users.noreply.github.com>
2021-05-18 10:10:05 +0100

    add image, username and caption to post

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import React, {useState} from 'react';
 import './App.css';
 import Post from './Post.js';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      username: "rhday",
+      caption: "It's working!",
+      imageUrl: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    },
+    {
+      username: "rhday",
+      caption: "It's working!",
+      imageUrl: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    }
+  ])
+
   return (
     <div className="app">
     <div className="app__header">
       <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
     </div>
     <h1>And so it begins...</h1>
-    <Post />
-    <Post />
-    <Post />
 
-      {/* Posts */}
-      {/* Posts */}
+    {
+      posts.map(post => (
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+      ))
+    }
+
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -2,20 +2,20 @@ import React from 'react';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({ username, caption, imageUrl}) {
     return (
         <div className="post">
             <div className="post__header">
                 <Avatar className="post__avatar" alt="Ruari" src="/static/images/avatar/1.jpg" />
-                <h3>Username</h3>
+                <h3>{username}</h3>
             </div>
             
             {/* Header --> avatar + username */}
 
-            <img className="post__image" src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" />
+            <img className="post__image" src={imageUrl} />
             {/* Image */}
 
-            <h4 className="post__text"><strong>rhday</strong> Building instaclone</h4>
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
             {/* Username + Caption */}
 
 
