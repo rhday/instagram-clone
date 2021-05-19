b57246469d68c884d09e11daf12a71e9eb0aa24d
rhday <54642787+rhday@users.noreply.github.com>
2021-05-19 14:14:53 +0100

    add modal and modal styling

--- src/App.js
+++ src/App.js
@@ -2,41 +2,68 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Post from './Post.js';
 import {db} from './firebase.js'
+import Modal from '@material-ui/core/Modal';
+import {makeStyles} from '@material-ui/core/styles';
+
+function getModalStyle() {
+  const top = 50
+  const left = 50
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
 
   const [posts, setPosts] = useState([]);
-  //  {
-  //    username: "rhday",
-  //    caption: "It's working!",
-  //    imageUrl: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-  //  },
-  //  {
-  //    username: "rhday",
-  //    caption: "It's working!",
-  //    imageUrl: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
-  //  }
-  //])
-
+  const [open, setOpen] = useState(false);
+  
   //useEffect - runs a piece of code based on a specific condition --> this is a hook
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
       //everytime a post is added this fires
-      setPosts(snapshot.docs.map(doc => doc.data()));
-    })
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id, post: doc.data()
+      })));
+    });
   }, []);
 
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
     <div className="app__header">
       <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
     </div>
     <h1>And so it begins...</h1>
 
     {
-      posts.map(post => (
-        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+      posts.map(({id, post}) => (
+        <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
       ))
     }
 
