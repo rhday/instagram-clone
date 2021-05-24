f31a0ddf87554f581d5b8f4d8145b42ae7a48a24
rhday <54642787+rhday@users.noreply.github.com>
2021-05-24 14:41:36 +0100

    continue to build uploadImage

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -29,6 +29,23 @@ function ImageUpload() {
                     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
                 );
                 setProgress(progress);
+            },
+            (error) => {
+                //Error function
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                //Complete function
+                storage.ref("images").child(image.name).getDownloadURL()
+                .then(url => {
+                    // post image inside the db
+                    db.collection("posts").add({
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                        caption: caption,
+                        imageUrl: url
+                    })
+                })
             }
         )
     }
