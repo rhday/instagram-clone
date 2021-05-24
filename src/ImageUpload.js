ad9b4315670a02fc24dfdc356cf7bf21396b29c9
rhday <54642787+rhday@users.noreply.github.com>
2021-05-24 10:46:32 +0100

    build ImageUpload

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -7,7 +7,11 @@ function ImageUpload() {
     const [progress, setProgress] = useState('0');
 
     const handleChange = (e) => {
-
+        {/* Select the first file that gets input */}
+        if (e.target.files[0]) {
+            {/* Set the image in state to that file */}
+            setImage(e.target.files[0]);
+        }
     }
 
     const handleUpload = (e) => {
@@ -19,7 +23,7 @@ function ImageUpload() {
             {/* Caption input */}
             {/* File Picker */}
             {/* Post Button */}
-            <Input type="text" placeholder="Enter you caption:" onChange={event => setCaption(event.target.value)} value={caption}/>
+            <Input type="text" placeholder="Enter your caption:" onChange={event => setCaption(event.target.value)} value={caption}/>
             <Input type="file" onChange={handleChange}/>
             <Button onClick={handleUpload}>Upload</Button>
         </div>
